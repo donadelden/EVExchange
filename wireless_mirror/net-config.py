effc4a26ca11e4493863dc69cd60b94a983b2016
Federico Turrin <federico.turrin94@gmail.com>
2021-05-14 06:44:23 -0400

    Fix net conf wireless_mirror

--- wireless_mirror/net-config.py
+++ wireless_mirror/net-config.py
@@ -10,66 +10,70 @@ from mn_wifi.net import Mininet_wifi
 
 
 def topology():
-    "Create a network."
-    net = Mininet_wifi()
+	"Create a network."
+	net = Mininet_wifi()
 
-    info("*** Creating nodes\n")
-    ap_arg = {'client_isolation': True}
-    ap1 = net.addAccessPoint('ap1', ssid="ap1", mode="g",
-                             channel="5",**ap_arg)
-    sta1 = net.addStation('sta1', ip='11.0.0.1/24')#, position='10,10,0')
-    sta2 = net.addStation('sta2', ip='11.0.0.2/24')#, position='20,20,0')
+	info("*** Creating nodes\n")
+	ap_arg = {'client_isolation': True}
+	ap1 = net.addAccessPoint('ap1', ssid="ap1", mode="g",
+							 channel="5",**ap_arg)
+	sta1 = net.addStation('sta1')#, position='10,10,0')
+	sta2 = net.addStation('sta2')#, position='20,20,0')
 
-    c0 = net.addController('c0')
-
-
-    ev1 = net.addHost( 'ev1', ip='10.0.0.10/24',
-                        #defaultRoute='via 10.1.0.1',
-                        mac="00:00:00:00:00:10")
-
-    se2 = net.addHost( 'se2', ip='10.0.0.21/24',
-                        #defaultRoute='via 10.1.0.2',
-                        mac="00:00:00:00:00:21")
+	c0 = net.addController('c0')
 
 
-    info("*** Configuring wifi nodes\n")
-    net.configureWifiNodes()
+	ev1 = net.addHost( 'ev1', ip='10.0.10.1/24',
+						#defaultRoute='via 10.1.0.1',
+						mac="00:00:00:00:00:10")
 
-    info("*** Associating Stations\n")
-    net.addLink(sta1, ap1)
-    net.addLink(sta2, ap1)
-
-    net.addLink(ev1, sta1, intfName2='sta1-eth0',
-                   params2={ 'ip' : '10.0.0.110/24'} )
-    #sta1.setMac('00:00:00:00:01:10', 'sta1-eth0') # doesn't work, thank you mininet wifi!
-    net.addLink(se2, sta2, intfName2='sta2-eth1',
-                   params2={ 'ip' : '10.0.0.221/24'} )
-    #sta2.setMac('00:00:00:00:02:21', 'sta2-eth1')
+	se2 = net.addHost( 'se2', ip='10.0.30.1/24',
+						#defaultRoute='via 10.1.0.2',
+						mac="00:00:00:00:00:21")
 
 
+	info("*** Configuring wifi nodes\n")
+	net.configureWifiNodes()
 
-    info("*** Starting network\n")
-    net.build()
-    c0.start()
-    ap1.start([c0])
+	info("*** Associating Stations\n")
+	net.addLink(sta1, ap1)
+	net.addLink(sta2, ap1)
+	net.addLink(ev1, sta1)
+	net.addLink(se2, sta2)
+	#net.addLink(ev1, sta1, intfName2='sta1-eth0', params2={ 'ip' : '10.0.0.110/24'} )
+	#sta1.setMac('00:00:00:00:01:10', 'sta1-eth0')
+	#net.addLink(se2, sta2, intfName2='sta2-eth1', #params2={ 'ip' : '10.0.0.221/24'} )
+	#sta2.setMac('00:00:00:00:02:21', 'sta2-eth1')
 
+	info("*** Starting network\n")
+	net.build()
+	c0.start()
+	ap1.start([c0])
 
-    ap1.cmd('ovs-ofctl add-flow ap1 "priority=0,arp,in_port=1,'
-            'actions=output:in_port,normal"')
-    ap1.cmd('ovs-ofctl add-flow ap1 "priority=0,icmp,in_port=1,'
-            'actions=output:in_port,normal"')
-    ap1.cmd('ovs-ofctl add-flow ap1 "priority=0,udp,in_port=1,'
-            'actions=output:in_port,normal"')
-    ap1.cmd('ovs-ofctl add-flow ap1 "priority=0,tcp,in_port=1,'
-            'actions=output:in_port,normal"')
+	sta1.cmd('ifconfig sta1-eth1 10.0.10.10 netmask 255.255.255.0 up')
+	sta1.cmd('ifconfig sta1-wlan0 10.0.20.10 netmask 255.255.255.0 up')
+	ev1.cmd('route add default gw 10.0.10.10')
 
-    info("*** Running CLI\n")
-    CLI(net)
+	sta2.cmd('ifconfig sta2-wlan0 10.0.20.2 netmask 255.255.255.0 up')
+	sta2.cmd('ifconfig sta2-eth1 10.0.30.10 netmask 255.255.255.0 up')
+	se2.cmd('route add default gw 10.0.30.10')
+	
+	ap1.cmd('ovs-ofctl add-flow ap1 "priority=0,arp,in_port=1,'
+			'actions=output:in_port,normal"')
+	ap1.cmd('ovs-ofctl add-flow ap1 "priority=0,icmp,in_port=1,'
+			'actions=output:in_port,normal"')
+	ap1.cmd('ovs-ofctl add-flow ap1 "priority=0,udp,in_port=1,'
+			'actions=output:in_port,normal"')
+	ap1.cmd('ovs-ofctl add-flow ap1 "priority=0,tcp,in_port=1,'
+			'actions=output:in_port,normal"')
 
-    info("*** Stopping network\n")
-    net.stop()
+	info("*** Running CLI\n")
+	CLI(net)
+
+	info("*** Stopping network\n")
+	net.stop()
 
 
 if __name__ == '__main__':
-    setLogLevel('info')
-    topology()
+	setLogLevel('info')
+	topology()
