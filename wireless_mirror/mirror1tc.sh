ca0c7b489f35e06fc2a00b53ff3226e8e45e0a6f
Denis <denisdonadel@gmail.com>
2021-05-14 14:50:35 +0200

    simple change to make the mirror work

--- wireless_mirror/mirror1tc.sh
+++ wireless_mirror/mirror1tc.sh
@@ -3,11 +3,11 @@
 # ref: https://backreference.org/2014/06/17/port-mirroring-with-linux-bridges/
 
 # estabilish qdisc to interfaces
-tc qdisc add dev sta1-eth0 ingress
+tc qdisc add dev sta1-eth1 ingress
 #tc qdisc add dev sta1-eth1 ingress
 
 # create the filter who match all and send to wlan0
-tc filter add dev sta1-eth0 parent ffff: \
+tc filter add dev sta1-eth1 parent ffff: \
     protocol all \
     u32 match u8 0 0 \
     action mirred egress mirror dev sta1-wlan0
