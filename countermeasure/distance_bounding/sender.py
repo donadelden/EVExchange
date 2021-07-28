74e6aba07708a79105f639db644e673b4d0212a8
Denis <denisdonadel@gmail.com>
2021-07-28 15:35:27 +0200

    Distance Bounding mechanisms added

---
+++ countermeasure/distance_bounding/sender.py
@@ -0,0 +1,80 @@
+import time
+import socket
+import numpy as np
+import random
+import string
+import os.path
+import sys
+
+def get_random_string(length):
+    letters = string.ascii_letters + string.digits
+    return ''.join(random.choice(letters) for _ in range(length))
+
+
+ITERATIONS = 1000
+
+for _ in range(0, ITERATIONS):
+    ip = "fe80::200:ff:fe00:22%ev1-eth0"  # change this when changing between hosts
+    # ip = "fe80::200:ff:fe00:22%ev2-eth0"  # for mitm but not attack
+    port = 5005
+
+    # Create socket for server
+    s = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM, 0)
+    address = ("::", port, 0, 2)
+    s.bind(address)
+    print("Starting...")
+    total_start = time.time()
+    deltas = []
+
+    TRIES = 102
+    DATA_SIZE = 1
+
+    data_to_be_sent = get_random_string(TRIES * DATA_SIZE)
+    s_ev = b""
+
+    # Let's send data through UDP protocol
+    for i in range(0, TRIES):
+        # retrieve the data to be sent
+        to_be_sent = data_to_be_sent[i*DATA_SIZE:(i+1)*DATA_SIZE].encode()
+
+        # rapid exchange
+        start = time.time()
+        s.sendto(to_be_sent, (ip, port, 0, 2))
+        data, _ = s.recvfrom(4)
+        end = time.time()
+
+        # create the final string
+        s_ev += to_be_sent + data
+        deltas.append(end-start)
+
+    # receive the string and check the correctness
+    s_se, _ = s.recvfrom(2024)
+    assert s_se == s_ev
+
+    # close the socket
+    s.close()
+
+    mu = np.mean(deltas[1:-1])
+    std = np.std(deltas[1:-1])
+    var = np.var(deltas[1:-1])
+    elapsed = time.time()-total_start
+    print(f"t: {elapsed}")
+    print(mu)
+    print(std)
+
+    BASE_PATH = "results/"
+    # PROP MODE DIST EXP
+    if sys.argv[1] == "LNS":
+        filename = BASE_PATH + f"attack-dist{sys.argv[3]}-exp{sys.argv[4]}-var{sys.argv[5]}-{sys.argv[1]}-{sys.argv[2]}.csv"
+    elif sys.argv[1] == "LDPL":
+        filename = BASE_PATH + f"attack-dist{sys.argv[3]}-exp{sys.argv[4]}-{sys.argv[1]}-{sys.argv[2]}.csv"
+    else:
+        filename = BASE_PATH + f"{sys.argv[1]}.csv"
+    if not os.path.isfile(filename):
+        with open(filename, "w") as f:
+            f.write("mean,std,variance,time_elapsed\n")
+
+    with open(filename, "a") as f:
+        f.write(f"{mu},{std},{var},{elapsed}\n")
+
+    time.sleep(0.3)
--- wireless/net-config.py
+++ wireless/net-config.py
@@ -2,16 +2,12 @@
 
 'EVExchange over wifi'
 
-import sys
-
 from mininet.log import setLogLevel, info
+from mininet.term import makeTerm
 from mn_wifi.cli import CLI
 from mn_wifi.net import Mininet_wifi
-
 from mn_wifi.v2g import EV, SE
-
 from time import sleep
-
 
 
 class Mininet_wifi_ext(Mininet_wifi):
@@ -19,7 +15,6 @@ class Mininet_wifi_ext(Mininet_wifi):
 	attack = True
 
 	def toggle_attack(self):
-		#global self.attack
 		if self.attack: 
 			info("*** EVExchange is active.\n")
 			info("*** Deactivating it...\n")
@@ -39,32 +34,36 @@ class Mininet_wifi_ext(Mininet_wifi):
 
 		self.terms.append(self.hosts[1].startCharge())
 		self.terms.append(self.hosts[3].startCharge())
-
 		sleep(5)
 
 		self.terms.append(self.hosts[0].charge(True))
-
 		sleep(7)
 
 		self.terms.append(self.hosts[2].charge(True))
-
 		sleep(7)
-
 
 
 def topology():
 	"Create a network."
+	# TEST PARAMETERS
+	MODE = "ac"  # "ac" "g"
+	DISTANCE = 10  # 2 10
+	PROP = "LNS"  # "LDPL" "LNS"
+	EXP = 4 # 2 - 4
+	VAR = 2
+
 	net = Mininet_wifi_ext()
 
 	info("*** Creating nodes\n")
 	ap_arg = {'client_isolation': True, 'position': '10,10,0'}
-	ap1 = net.addAccessPoint('ap1', ssid="ap1", mode="g", 
-							 channel="5", **ap_arg)
+	if MODE == "g":
+		ap1 = net.addAccessPoint('ap1', ssid="ap1", mode="g", channel="5", **ap_arg)
+	elif MODE == "ac":
+		ap1 = net.addAccessPoint('ap1', ssid="ap1", mode="ac", channel="36", **ap_arg)
 	sta1 = net.addStation('sta1', position='10,10,0')
-	sta2 = net.addStation('sta2', position='200,10,0')
+	sta2 = net.addStation('sta2', position=f'{10+DISTANCE},10,0')
 
 	c0 = net.addController('c0')
-
 
 	ev1 = net.addHost( 'ev1', cls=EV, ip='10.1.10.1/24',
 						mac="00:00:00:00:00:11")
@@ -77,6 +76,12 @@ def topology():
 
 	se2 = net.addHost( 'se2', cls=SE, ip='10.2.20.1/24',
 						mac="00:00:00:00:00:22")	
+
+	info("*** Configuring Propagation Model\n")
+	if PROP == "LDPL":
+		net.setPropagationModel(model="logDistance", exp=EXP)  #LDPL
+	elif PROP == "LNS":
+		net.setPropagationModel(model="logNormalShadowing", exp=EXP, variance=VAR)  #LNS
 
 	info("*** Configuring wifi nodes\n")
 	net.configureWifiNodes()
@@ -93,6 +98,13 @@ def topology():
 	net.build()
 	c0.start()
 	ap1.start([c0])
+
+	# Problem. MiniV2G manage the certificates only between two entities. 
+	# TODO: find a way to enable an EV to TLS-charge from different SEs
+	TLS = False
+	if TLS:
+		ev1.setTLS(True, generate_certificates=True, SECC=se2)
+		ev2.setTLS(True, generate_certificates=True, SECC=se1)
 
 	# 1s side
 	sta1.cmd('ifconfig sta1-wlan0 10.0.20.10 netmask 255.255.255.0 up')
@@ -130,7 +142,22 @@ def topology():
 		info("** Starting bridges (EVExchange NOT activated")
 		sta1.cmd("./bridge1.sh")
 		sta2.cmd("./bridge2.sh")
-	
+
+	# method to run measurements for the distance bounding
+	def run_measurements():
+		xterm = False  # decide if you want to keep a terminal open in the end
+		if xterm:
+			xterm = "; bash - i"
+		else:
+			xterm = ""
+		se2_cmd = "cd ../countermeasure/distance_bounding/ && python3 receiver.py"
+		ev1_cmd = f"cd ../countermeasure/distance_bounding/ && python3 sender.py {PROP} {MODE} {DISTANCE} {EXP} {VAR}"
+		sleep(1)
+		makeTerm(se2, cmd=f"bash -i -c '{se2_cmd}{xterm}'")
+		sleep(0.2)
+		makeTerm(ev1, cmd=f"bash -i -c '{ev1_cmd}{xterm}'")
+
+	#run_measurements()
 
 	info("*** Running CLI\n")
 	CLI(net)
